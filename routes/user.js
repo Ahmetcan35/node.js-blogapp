c4ac3c266d1e4eb8964bb4047109615f3936bf0e
Ahmetcan35 <ahmetcancetinz@gmail.com>
2024-09-10 20:21:40 +0300

    SQL eğitimi sonu

--- routes/user.js
+++ routes/user.js
@@ -2,69 +2,82 @@ const express = require("express");
 const router  = express.Router();
 const db = require("../data/db");
 
-const data ={
-
-    title:"Popüler Kurslar",
-    categories:["Tüm Programlar","Web Geliştirme","Python Programlama","Mobil Programlama","Veri Analizi"],
-    blogs:[{
-        blogid:1,
-        blogtitle:"Komple Uygulamalı Web Geliştirme",
-        blogdetail:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-        resim:"1.jpeg",
-        anasayfa:true,
-    },
-    {
-        blogid:2,
-        blogtitle:"Python ile Sıfırdan İleri Seviye Python Programlama",
-        blogdetail:"Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-        resim:"2.jpeg",
-        anasayfa:true,
-    },
-    {
-        blogid:3,
-        blogtitle:"Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-        blogdetail:"Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-        resim:"3.jpeg",
-        anasayfa:false,
-    },{
-        blogid:2,
-        blogtitle:"Python ile Sıfırdan İleri Seviye Python Programlama",
-        blogdetail:"Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-        resim:"2.jpeg",
-        anasayfa:true,
-    },]
+router.use("/blogs/category/:categoryid",async function(req, res) {
+    const id = req.params.categoryid;
+    try {
+    const [blogs, ] = await db.execute("select * from blog WHERE categoryid = ?",[id]);
+    const [categories, ] = await db.execute("select * from category");
+    res.render("users/blogs",{
+        blogs:blogs,
+        title:categories[id-1].categoryname,
+        categories:categories,
+        selectedCategory : id,
+        
+});
+console.log(title); 
+    
+} catch (err) {
+    console.log(err);
 }
-
-router.use("/blogs/:blogid", function(req, res) {
-    res.render("users/blog-details");
+    
 });
 
-router.use("/blogs", function(req, res) {
-    db.execute("select * from blog")
-    .then(result => {
-        console.log(result[0]);
+router.use("/blogs/:blogid", async function(req, res) {
+
+    const id = req.params.blogid;
+    try {
+        const [blog, ] =  await db.execute("select * from blog where blogid = ?",[id]);
+        
+        if(blog[0]){
+            return res.render("users/blog-details",{
+            title:blog[1].blogtitle,
+            blog : blog[0],
+        });}
+
+        res.redirect("/");
+
+            
+    } catch (err) {
+        console.log(err);
+    }
+    
+});
+
+router.use("/blogs", async function(req, res) {
+
+    try {
+        const [blogs, ] = await db.execute("select * from blog WHERE onay = 1");
+        const [categories] = await db.execute("select * from category");
         res.render("users/blogs",{
-            blogs:result[0],
-            title:"Tüm Kurslar",
-            categories:data.categories,
+            blogs:blogs,
+            title:"Tüm  Kurslar",
+            categories:categories,
+            selectedCategory : null,
 
         });
-    })
-    .catch(err => console.log(err));
+        
+    } catch (err) {
+        console.log(err);
+    }
+    
 });
 
-router.use("/", function(req, res) {
-    db.execute("select * from blog")
-    .then(result => {
-        console.log(result[0]);
+router.use("/", async function(req, res) {
+
+    try {
+        const [blogs, ] = await db.execute("select * from blog WHERE onay = 1 And anasayfa =1");
+        const [categories, ] = await db.execute("select * from category");
         res.render("users/index",{
-            blogs:result[0],
+            blogs:blogs,
             title:"Popüler Kurslar",
-            categories:data.categories,
+            categories:categories,
+            selectedCategory : null,
 
         });
-    })
-    .catch(err => console.log(err));
+        
+    } catch (err) {
+        console.log(err);
+    }
     
 });
 
