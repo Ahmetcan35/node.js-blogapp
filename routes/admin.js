531889be2dfb72a6155a008764d56b79fd0d924d
Ahmetcan35 <ahmetcancetinz@gmail.com>
2024-09-12 20:37:39 +0300

    silme sayfasının eklenmesi

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,36 @@ const router  = express.Router();
 
 const db = require("../data/db");
 
+router.get("/blog/delete/:blogid", async function (req, res) {
 
+    const blogid = req.params.blogid;
+
+    try {
+    const [blogs, ] = await db.execute("SELECT * FROM blog Where blogid=?",[blogid]);
+    const blog = blogs[0]; 
+    res.render("admin/blog-delete",{
+        title:"delete blog",
+        blog : blog,
+        
+    });   
+    } catch (err) {
+        console.log(err);
+    }
+    
+});
+
+router.post("/blog/delete/:blogid",async function (req, res) {
+    const blogid= req.body.blogid;
+    try {
+        await db.execute("DELETE FROM blog WHERE blogid=?",[blogid]);
+        res.redirect("/admin/blogs")
+
+        
+    } catch (err) {
+        console.log(err);
+    }
+    
+});
 
 router.get("/blog/create", async function(req, res) {
     try {
@@ -55,7 +84,23 @@ router.get("/blogs/:blogid", async function(req, res) {
     } catch (err) {
         console.log(err);
     }
-});
+});//hazır blog detaili getirme
+
+router.post("/blogs/:blogid", async function (req, res) {
+        const blogid= req.body.blogid;
+        const baslik= req.body.baslik;
+        const aciklama= req.body.aciklama;
+        const resim= req.body.resim;
+        const anasayfa= req.body.anasayfa == "on" ? 1:0;       
+        const onay= req.body.onay == "on" ? 1:0;  
+        const kategori =req.body.kategori;
+    try {
+        await  db.execute("UPDATE blog SET blogtitle=?,blogdetail=?,resim=?,anasayfa=?,onay=?,categoryid=? WHERE blogid=?",[baslik,aciklama,resim,anasayfa,onay,kategori,blogid]);
+        res.redirect("/admin/blogs");
+    } catch (err) {
+        console.log(err);
+    }
+})//blog güncelleme 
 
 router.get("/category/create", function(req, res) {
     res.render("admin/blog-listasd");
