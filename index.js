a277f1d60b7be75e6c3b84f68c91464b1ba6e9b8
Ahmetcan35 <ahmetcancetinz@gmail.com>
2024-09-12 04:08:40 +0300

    Create ve Güncelleme Sayfaları

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 app.set("view engine","ejs");
+app.use(express.urlencoded({ extended: false }));   
 console.log(app.get("view engine"));
 
 const path = require("path");
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,81 @@
 const express = require("express");
 const router  = express.Router();
 
-router.use("/blog/create", function(req, res) {
-    res.render("admin/blog-create");
+const db = require("../data/db");
+
+
+
+router.get("/blog/create", async function(req, res) {
+    try {
+
+        const [categories, ] = await db.execute("select * from category");
+        res.render("admin/blog-create",{
+            title:"Tüm Kurslar",
+            categories:categories,
+        });  
+    } catch (err) {
+        console.log(err);
+    }
+}); ///admin create sayfası
+router.post("/blog/create",  async function (req, res) {
+    const baslik = req.body.baslik;
+    const aciklama = req.body.aciklama;
+    const resim = req.body.resim;
+    const kategori = req.body.kategori;
+    const anasayfa = req.body.anasayfa == "on" ? 1:0;
+    const onay = req.body.onay == "on" ? 1:0;
+    try {
+        await db.execute("INSERT INTO blog (blogtitle,blogdetail,resim,categoryid,anasayfa,onay) VALUES (?,?,?,?,?,?)", [baslik,aciklama,resim,kategori,anasayfa,onay]);
+        res.redirect("/admin/blogs");
+
+
+    } catch (err) {
+        console.log(err);
+    }
+}); ///admin create sayfası
+
+router.get("/blogs/:blogid", async function(req, res) {
+
+    const blogid = req.params.blogid;
+
+    try {
+        const [blogs, ]= await db.execute("select * from blog where blogid= ?",[blogid]);
+        const [categories, ] = await db.execute("select * from category");
+        const blog = blogs[0];
+        if(blog){
+            res.render("admin/blog-edit",{
+                title:"blog.baslik",
+                blog :blog,
+                categories: categories,
+    
+    
+            });
+        }
+        res.redirect("/admin/blogs");
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-router.use("/blogs/:blogid", function(req, res) {
-    res.render("admin/blog-edit");
+router.get("/category/create", function(req, res) {
+    res.render("admin/blog-listasd");
 });
-router.use("/blogs", function(req, res) {
-    res.render("admin/blog-list");
+router.get("/category", function(req, res) {
+    res.render("admin/blog-listsa");
 });
+router.get("/blogs", async function(req, res) {
+    try {
+        const [blogs, ]= await db.execute("SELECT blogid,blogtitle,resim from blog ");
+        
+        res.render("admin/blog-list",{
+            title:"Blog List",
+            blogs:blogs
+        });
+        
+    } catch (err) {
+        console.log(err);
+    }
+});// admin blog listesi
 
 
 module.exports = router;
